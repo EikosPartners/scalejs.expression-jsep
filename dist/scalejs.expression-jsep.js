a9ecc06acd9414df61a4d223b1b4026c6a91ada0
Zachary Donato <zdonato@stevens.edu>
2016-05-25 09:20:27 -0400

    Updated to 0.5.0 and converted scalejs!

--- dist/scalejs.expression-jsep.js
+++ dist/scalejs.expression-jsep.js
@@ -1,7 +1,6 @@
 
-
 define('scalejs.expression-jsep',[
-    'scalejs!core',
+    'scalejs.core',
     'jsep',
     'knockout'
 ], function (
--- rjsconfig.js
+++ rjsconfig.js
@@ -6,9 +6,14 @@ var require = {
       '<%=ext_name%>'
     ]
   },
+  map: {
+    '*': {
+      'scalejs.core': 'empty:'
+    }
+  },
   paths: {
     requirejs: '../bower_components/requirejs/require',
-    scalejs: '../bower_components/scalejs/dist/scalejs.min',
+    scalejs: '../bower_components/scalejs/dist/scalejs',
     jsep: '../bower_components/jsep/build/jsep',
     knockout: '../bower_components/knockout/dist/knockout',
     almond: '../bower_components/almond/almond',
@@ -16,7 +21,10 @@ var require = {
     'scalejs.functional': '../bower_components/scalejs.functional/dist/scalejs.functional.min',
     'scalejs.metadata-factory': '../bower_components/scalejs.metadata-factory/dist/scalejs.metadata-factory',
     'scalejs.mvvm': '../bower_components/scalejs.mvvm/dist/scalejs.mvvm',
-    text: '../bower_components/text/text'
+    text: '../bower_components/text/text',
+    'scalejs.application': '../bower_components/scalejs/src/scalejs.application',
+    'scalejs.core': '../bower_components/scalejs/src/scalejs.core',
+    'scalejs.sandbox': '../bower_components/scalejs/src/scalejs.sandbox'
   },
   packages: [
 
